8351568ebe6e2bc9d7987ca1f7c900b35c4a5d48
Josh Smith <racersmith@gmail.com>
2017-06-18 09:56:06 -0700

    Add global kinematic model Add initial starting values for dt and N

--- mpc_to_line/src/MPC.cpp
+++ mpc_to_line/src/MPC.cpp
@@ -11,8 +11,8 @@ namespace plt = matplotlibcpp;
 using CppAD::AD;
 
 // TODO: Set N and dt
-size_t N = ? ;
-double dt = ? ;
+size_t N = 25 ;
+double dt = 0.05 ;
 
 // This value assumes the model presented in the classroom is used.
 //
@@ -80,10 +80,22 @@ class FG_eval {
     // The rest of the constraints
     for (int t = 1; t < N; t++) {
       AD<double> x1 = vars[x_start + t];
+      AD<double> y1 = vars[y_start + t];
+      AD<double> psi1 = vars[psi_start + t];
+      AD<double> v1 = vars[v_start + t];
+      AD<double> cte1 = vars[cte_start + t];
+      AD<double> epsi1 = vars[epsi_start + t];
 
       AD<double> x0 = vars[x_start + t - 1];
+      AD<double> y0 = vars[y_start + t - 1];
       AD<double> psi0 = vars[psi_start + t - 1];
       AD<double> v0 = vars[v_start + t - 1];
+      AD<double> cte0 = vars[cte_start + t -1];
+      AD<double> epsi0 = vars[epsi_start + t -1];
+
+      AD<double> a0 = vars[a_start + t -1];
+      AD<double> delta0 = vars[delta_start + t -1];
+
 
       // Here's `x` to get you started.
       // The idea here is to constraint this value to be 0.
@@ -94,6 +106,17 @@ class FG_eval {
 
       // TODO: Setup the rest of the model constraints
       fg[1 + x_start + t] = x1 - (x0 + v0 * CppAD::cos(psi0) * dt);
+      fg[1 + y_start + t] = y1 - (y0 + v0 * CppAD::sin(psi0) * dt);
+      fg[1 + psi_start + t] = psi1 - (psi0 + v0/Lf * delta0 * dt);
+      fg[1 + v_start + t] = v1 - (v0 + a0*dt);
+
+      // Target position from path fit
+      AD<double> f0 = coeffs[1]*x0 + coeffs[0];
+      fg[1 + cte_start + t] = cte1 - ((f0 - y0) + v0*CppAD::sin(epsi0)*dt);
+
+      // Target heading from path fit
+      AD<double> psides0 = CppAD::atan(coeffs[1]);
+      fg[1 + epsi_start + t] = epsi1 - ((psi0 - psides0) + v0*delta0/Lf*dt);
     }
   }
 };
@@ -263,7 +286,7 @@ int main() {
   ptsy << -1, -1;
 
   // TODO: fit a polynomial to the above x and y coordinates
-  auto coeffs = ? ;
+  auto coeffs = polyfit(ptsx, ptsy, 1) ;
 
   // NOTE: free feel to play around with these
   double x = -1;
@@ -271,9 +294,9 @@ int main() {
   double psi = 0;
   double v = 10;
   // TODO: calculate the cross track error
-  double cte = ? ;
+  double cte = polyeval(coeffs, 0) - y;
   // TODO: calculate the orientation error
-  double epsi = ? ;
+  double epsi = atan(coeffs[1]) - psi;
 
   Eigen::VectorXd state(6);
   state << x, y, psi, v, cte, epsi;
